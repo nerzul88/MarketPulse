2483816ad40c361a81b76d9f51939f24d636b0a4
nerzul88 <nerzul.88@gmail.com>
2026-03-22 15:47:42 +0300

    Project added

---
+++ MarketPulse/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarketPulse
+//
+//  Created by Александр Касьянов on 22.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
